f9c5aa4909ac9dddb7dca28bcd2ba84a127ddd00
Moses-odessa <moses2804@gmail.com>
2018-09-05 11:49:47 +0300

    test

--- App.js
+++ App.js
@@ -1,88 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
-import {UIActivityIndicator} from 'react-native-indicators';
+
+import {CategoriesPage} from './categories-page'
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      groupsJSON: [],
-      isLoading: true
-    };
-  }
-
-  fetchGroupsJSON() {
-    var url = 'http://homey.in.ua/api/categories/?output_format=JSON&display=[id,name,id_parent]';
-    fetch(url,
-      { 
-        method: 'get', 
-        headers: new Headers({
-          'Authorization': 'Basic '+btoa('CM67C92UKYFPE2U7837YH4HGDBV78FBH:'), 
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }), 
-        body: ''
-      })
-      .then( response => response.json() )
-      .then( jsonData => {
-        console.log(jsonData);
-        this.setState({isLoading: false});
-      })
-	.catch( error => console.log('Fetch error ' + error) );
-  }
-
-  componentDidMount() {
-    this.fetchGroupsJSON();
-  }
+  } 
 
   render() {
-    var {isLoading} = this.state;
-    if(isLoading)
-      return this.renderLoadingMessage();
-    else
-      return this.renderResults();
+    console.log('Starting render');
+      <React.NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Categories',
+          component: CategoriesPage,
+        }}/>
   }
 
-  renderLoadingMessage() {
-    return (
-      
-  <View style={styles.loadingContainer}>
-        <UIActivityIndicator color={'#fff'}
-          size={60} />
-        <Text style={color={'#fff'}}>
-          Data loading
-        </Text>
-   </View>
-    );
-  }
-
-  renderResults() {
-    return (
-      
-  <View style={styles.loadingContainer}>
-        <Text style={color={'#fff'}}>
-          Data loaded
-        </Text>
-       
-  </View>
-    );
-  }
 }
 
-const styles = StyleSheet.create({
+const styles = React.StyleSheet.create({  
   container: {
-    flex: 1,
-    backgroundColor: '#981',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  loadingContainer: {
-    flex: 1,
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#000'
-    },
-loadingIndicator: {
-}
+    flex: 1
+  }
 });
