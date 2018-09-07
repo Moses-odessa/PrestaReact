bbcbbbb9f04816d5c249a866401daeb96864214f
Moses-odessa <moses2804@gmail.com>
2018-09-07 11:41:26 +0300

    add CategoriesPage

--- App.js
+++ App.js
@@ -1,26 +1,73 @@
 import React from 'react';
+import { Button, View, Text } from 'react-native';
+import { createStackNavigator } from 'react-navigation'; 
+import Categories from './src/Categories';
 
-import {CategoriesPage} from './categories-page'
-
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-  } 
+class DetailsScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: navigation.getParam('otherParam', 'A Nested Details Screen'),
+    };
+  };
 
   render() {
-    console.log('Starting render');
-      <React.NavigatorIOS
-        style={styles.container}
-        initialRoute={{
-          title: 'Categories',
-          component: CategoriesPage,
-        }}/>
-  }
+    /* 2. Get the param, provide a fallback value if not available */
+    const { navigation } = this.props;
+    const itemId = navigation.getParam('itemId', 'NO-ID');
+    const otherParam = navigation.getParam('otherParam', 'some default value');
 
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Text>itemId: {JSON.stringify(itemId)}</Text>
+        <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() =>
+            this.props.navigation.push('Details', {
+              itemId: Math.floor(Math.random() * 100),
+            })}
+        />
+        <Button
+          title="Update the title"
+          onPress={() =>
+            this.props.navigation.setParams({ otherParam: 'Updated!' })}
+        />
+        <Button
+          title="Go to Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
+      </View>
+    );
+  }
 }
 
-const styles = React.StyleSheet.create({  
-  container: {
-    flex: 1
+const RootStack = createStackNavigator(
+  {
+    Home: Categories,
+    Details: DetailsScreen,
+  },
+  {
+    initialRouteName: 'Home',
+    /* The header config from HomeScreen is now here */
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: '#f4511e',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
   }
-});
+);
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
